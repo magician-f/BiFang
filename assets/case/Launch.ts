f2816759d562087cb46fff8c5347128b3c45d58f
knifestone <378741819@qq.com>
2019-08-21 20:59:47 +0800

    新增微信的友情小程序跳转

---
+++ assets/case/Launch.ts
@@ -0,0 +1,46 @@
+// Learn TypeScript:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/typescript.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const ToItem = function(name,scene){
+    return {
+        name:name,
+        scene:scene
+    }
+}
+const {ccclass, property} = cc._decorator;
+@ccclass
+export default class Launch extends cc.Component {
+
+    @property(cc.Node)
+    nodeContainer: cc.Node = null
+
+    @property(cc.Node)
+    btnTemplate: cc.Node = null
+
+    onLoad () {
+        this.btnTemplate.parent = null
+        let arr = [
+            ToItem("微信友情跳转","WXAppScene")
+        ]
+        for(let i=0;i<arr.length;i++){
+            let bean = arr[i]
+            let btnTemplate = cc.instantiate(this.btnTemplate)
+            this.nodeContainer.addChild(btnTemplate)
+            btnTemplate.name = bean["scene"]
+            cc.find("Background/Label",btnTemplate).getComponent(cc.Label).string = bean["name"]
+        }
+    }
+
+    clickTemplate(event){
+        let name = event.target.name
+        cc.director.loadScene(name)
+    }
+
+}
